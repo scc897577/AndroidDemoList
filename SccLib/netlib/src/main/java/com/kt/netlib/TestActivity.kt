f3724e61680ba1330f92c4554cc735ec70931b1e
chaochao.sun <chaochao.sun@croninfo.com>
2019-10-11 14:30:28 +0800

    a

---
+++ SccLib/netlib/src/main/java/com/kt/netlib/TestActivity.kt
@@ -0,0 +1,15 @@
+package com.kt.netlib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+
+
+
+}
